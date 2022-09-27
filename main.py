66b13c60e31ed0d55f71802bd4064f5c030774b9
JurateCepaitiene <jurate.cepaitiene@gmail.com>
2022-09-27 20:33:51 +0300

    trying deploy Heroku 6

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def index():
     return 'Web App with Python Flask!'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=33507. debug=True)
